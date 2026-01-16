af61f6a62c741e9ba057701da5a9ffe5f4ee765f
Daniel1464 <Daniel_Chen@caryacademy.org>
2026-01-15 20:59:21 -0500

    Remove redundancy

--- src/main/java/robot/subsystems/intakepivot/PivotConsts.java
+++ src/main/java/robot/subsystems/intakepivot/PivotConsts.java
@@ -2,11 +2,9 @@ package robot.subsystems.intakepivot;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.measure.LinearVelocity;
 import lombok.experimental.FieldDefaults;
 
 import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecond;
 
 @FieldDefaults(makeFinal = true)
 public class PivotConsts {
@@ -15,5 +13,4 @@ public class PivotConsts {
     static double CURRENT_LIMIT_AMPS = 60;
     static Distance PIVOT_LENGTH = Meters.of(0.3);
     static DCMotor MOTOR_KIND = DCMotor.getNEO(1);
-    static LinearVelocity MAX_VEL = MetersPerSecond.of(2);
 }
