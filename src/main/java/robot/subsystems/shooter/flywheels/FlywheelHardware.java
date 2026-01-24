ca508f85fe5b66716dd03ab93cd4aaeb64eab5b6
michaelw28116 <Michael_wei@caryacademy.org>
2026-01-24 18:08:47 -0500

    Created methods for Sim & Kraken fly wheel hardware

--- src/main/java/robot/subsystems/shooter/flywheels/FlywheelHardware.java
+++ src/main/java/robot/subsystems/shooter/flywheels/FlywheelHardware.java
@@ -1,4 +1,14 @@
 package robot.subsystems.shooter.flywheels;
 
 public class FlywheelHardware {
+
+    public void setVoltage(double volts) {
+
+    }
+
+    public void setVelocity(double radiansPerSecond){
+
+    }
+
+
 }
--- src/main/java/robot/subsystems/shooter/flywheels/KrakenFlywheelHardware.java
+++ src/main/java/robot/subsystems/shooter/flywheels/KrakenFlywheelHardware.java
@@ -1,4 +1,19 @@
 package robot.subsystems.shooter.flywheels;
 
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 public class KrakenFlywheelHardware extends FlywheelHardware {
+    private final TalonFX talon = new TalonFX(/*id */ 1);
+    private final VoltageOut voltageRequest = new VoltageOut(0.0);
+
+    @Override
+    public void setVoltage(double volts) {
+        talon.setControl(voltageRequest.withOutput(volts));
+    }
+
+    @Override
+    public void setVelocity(double radiansPerSecond) {
+        super.setVelocity(radiansPerSecond);
+    }
 }
