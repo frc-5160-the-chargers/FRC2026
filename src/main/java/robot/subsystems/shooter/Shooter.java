7d9254a9c6ab0c2bfd9e40347ed7e35289124604
Daniel1464 <Daniel_Chen@caryacademy.org>
2026-01-26 08:47:12 -0500

    add examples to shooter

--- src/main/java/robot/subsystems/shooter/Shooter.java
+++ src/main/java/robot/subsystems/shooter/Shooter.java
@@ -1,10 +1,20 @@
 package robot.subsystems.shooter;
 
+import lib.RobotMode;
 import robot.subsystems.ChargerSubsystem;
+import robot.subsystems.common.PivotHardware;
 import robot.subsystems.shooter.flywheels.FlywheelHardware;
+import robot.subsystems.shooter.flywheels.KrakenFlywheelHardware;
+import robot.subsystems.shooter.flywheels.SimFlywheelHardware;
 
 public class Shooter extends ChargerSubsystem {
-    // TODO write implementations
-    private FlywheelHardware flywheelIO;
-    private HoodHardware hoodIO;
+    private FlywheelHardware flywheelIO = switch (RobotMode.get()) {
+        case REAL -> new KrakenFlywheelHardware();
+        case SIM -> new SimFlywheelHardware();
+        case REPLAY -> new FlywheelHardware();
+    };
+    private PivotHardware hoodIO = switch (RobotMode.get()) {
+        case REAL -> new HoodHardware();
+        case SIM, REPLAY -> new PivotHardware();
+    };
 }
